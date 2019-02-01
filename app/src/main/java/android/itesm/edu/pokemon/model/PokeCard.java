8ca055028d8e1ea1b8f77d4d33f3abd48b86af81
Charlhye <carlosbmtz@gmail.com>
2019-02-01 12:21:01 -0600

    Update final

--- app/src/main/java/android/itesm/edu/pokemon/model/PokeCard.java
+++ app/src/main/java/android/itesm/edu/pokemon/model/PokeCard.java
@@ -1,17 +1,19 @@
 package android.itesm.edu.pokemon.model;
 
-public class PokeCard {
+import java.io.Serializable;
 
-    private String id, name, imageUrl, artist;
+public class PokeCard implements Serializable {
+    private String id, name, imageURL, author;
 
-    public PokeCard() {
-    }
-
-    public PokeCard(String id, String name, String imageUrl, String artist) {
+    public PokeCard(String id, String name, String imageURL, String author) {
         this.id = id;
         this.name = name;
-        this.imageUrl = imageUrl;
-        this.artist = artist;
+        this.imageURL = imageURL;
+        this.author = author;
+    }
+
+    public PokeCard(){
+
     }
 
     public String getId() {
@@ -30,19 +32,19 @@ public class PokeCard {
         this.name = name;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public String getImageURL() {
+        return imageURL;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 
-    public String getArtist() {
-        return artist;
+    public String getAuthor() {
+        return author;
     }
 
-    public void setArtist(String artist) {
-        this.artist = artist;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
